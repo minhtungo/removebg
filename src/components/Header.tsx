e1dc6eb3d58df1f0e67a15879b8709e341c6cd93
Minh Tu Ngo <loducfc@gmail.com>
2023-03-06 19:25:19 -0500

    style: add header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,33 @@
 import { FcEditImage } from 'react-icons/fc';
+import { FiGithub } from 'react-icons/fi';
+import { useState, useEffect } from 'react';
 
 const Header = () => {
+  const [top, setTop] = useState(true);
+
+  useEffect(() => {
+    const scrollHandler = () => {
+      window.pageYOffset > 10 ? setTop(false) : setTop(true);
+    };
+    window.addEventListener('scroll', scrollHandler);
+    return () => window.removeEventListener('scroll', scrollHandler);
+  }, []);
+
   return (
-    <header className='fixed w-full z-10 transition duration-300 ease-in-out backdrop-filter'>
-      <div className='max-w-6xl mx-auto px-4 flex items-center justify-between h-16 md:h-20 border-b border-gray-500/20'>
-        <div className='cursor-pointer flex items-center select-none'>
-          <FcEditImage className='h-8 w-8' />
-          <span className='text-2xl font-bold ml-2'>removebg</span>
+    <header
+      className={`fixed z-10 w-full backdrop-filter transition duration-300 ease-in-out ${
+        !top && 'bg-gray-800/20 shadow-lg backdrop-blur-sm'
+      }`}
+    >
+      <div className='mx-auto flex h-16 max-w-6xl items-center justify-between border-b border-gray-500/20 px-4 md:h-20'>
+        <div className='flex cursor-pointer select-none items-center justify-center'>
+          <FcEditImage className='h-6 w-6 sm:h-8 sm:w-8' />
+          <span className='ml-2 text-lg font-bold sm:text-xl'>removebg</span>
+        </div>
+        <div className='flex items-center'>
+          <a href='https://github.com/minhtungo' target='_blank'>
+            <FiGithub className='h-5 w-5 hover:text-blue-500' />
+          </a>
         </div>
       </div>
     </header>
